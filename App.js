acc320176d41da89dfb91d6c182c6e1ff2ab3cf2
Harjap Singh <157319482+Harjap-Sngh@users.noreply.github.com>
2025-01-24 22:21:22 -0700

    initial-layout setup

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [input, setInput] = useState("");
+
+  let info = [
+    "AC",
+    "+/-",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "X",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "0",
+    ".",
+    "=",
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.screen} placeholder="0" />
+      <FlatList
+        style={styles.content}
+        data={info}
+        renderItem={({ item }) => <Text style={styles.text}>{item}</Text>}
+        numColumns={4}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +43,34 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+    paddingTop: 60,
+  },
+  content: {
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    width: "100%",
+  },
+  text: {
+    fontSize: 30,
+    textAlign: "center",
+    padding: 20,
+    margin: 5,
+    width: 80,
+    height: 80,
+    backgroundColor: "lightgray",
+    borderRadius: 10,
+  },
+  screen: {
+    height: "250",
+    width: "100%",
+    backgroundColor: "gray",
+    padding: 20,
+    margin: 20,
   },
 });
